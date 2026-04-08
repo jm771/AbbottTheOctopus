844dc2e9337c32eed5d12e522723190a04582df7
Audrey Gu <audreymgu@gmail.com>
2026-04-07 22:30:00 -0400

    Separate displayio test.

---
+++ eyes/eye_controllers_displayio.py
@@ -0,0 +1,31 @@
+import busio
+import digitalio
+from board import SCK, SCK_1, MOSI, MOSI_1, D25, D8, D27, D16, D26, D6
+
+
+from adafruit_rgb_display.gc9a01a import GC9A01A
+
+def make_left_eye_display():
+    # Configuration for CS and DC pins:
+    CS_PIN = D8
+    DC_PIN = D25
+    RST_PIN = D27
+
+    spi = busio.SPI(clock=SCK, MOSI=MOSI)
+
+    display = GC9A01A(spi, cs=digitalio.DigitalInOut(CS_PIN),
+                            dc=digitalio.DigitalInOut(DC_PIN), rst=digitalio.DigitalInOut(RST_PIN))
+    
+    return display
+
+def make_right_eye_display():
+    # Configuration for CS and DC pins:
+    CS_PIN = D16
+    DC_PIN = D26
+    RST_PIN = D6
+
+    spi = busio.SPI(clock=SCK_1, MOSI=MOSI_1)
+    display = GC9A01A(spi, cs=digitalio.DigitalInOut(CS_PIN),
+                            dc=digitalio.DigitalInOut(DC_PIN), rst=digitalio.DigitalInOut(RST_PIN))
+    
+    return display
--- eyes/sample_image_displayio.py
+++ eyes/sample_image_displayio.py
@@ -1,10 +1,26 @@
 
 import time
-from eyes.eye_controllers import make_left_eye_display, make_right_eye_display
+from eyes.eye_controllers_displayio import make_left_eye_display, make_right_eye_display
 from adafruit_rgb_display import color565
+
+# Import adafruit image management library
+import displayio
+import adafruit_imageload
 
 display1 = make_left_eye_display()
 display2 = make_right_eye_display()
+
+# # Is this a reset command?
+displayio.release_displays()
+
+# Create display group
+blinka_group = displayio.Group()
+bitmap, palette = adafruit_imageload.load("blinka_round.bmp",
+                                           bitmap=displayio.Bitmap,
+                                           palette=displayio.Palette)
+
+grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+blinka_group.append(grid)
 
 # Main loop:
 while True:
@@ -23,6 +39,9 @@ while True:
     display1.pixel(120, 160, color565(255, 0, 0))
     display2.pixel(120, 160, color565(255, 0, 0))
     # Pause 2 seconds.
+    time.sleep(2)
+    display1.root_group = blinka_group
+    display2.root_group = blinka_group
     time.sleep(2)
 
 
