66dc8a90897a298616cf010efdfc799c139cdb9e
JackMills <JackMills@NotRealEmail.com>
2026-04-09 11:23:42 -0400

    basic move to target

--- eyes/moving_eyes.py
+++ eyes/moving_eyes.py
@@ -2,6 +2,8 @@
 import time
 from eye_controllers import make_left_eye_display, make_right_eye_display
 from PIL import Image
+import random
+import math
 
 display1 = make_left_eye_display()
 display2 = make_right_eye_display()
@@ -19,13 +21,30 @@ x = scaled_width // 2 - width // 2
 y = scaled_height // 2 - height // 2
 image1 = image1.crop((x, y, x + width, y + height))
 
-i = 0
+currentX = 0
+currentY = 0
+targetX = 100
+targetY = 100
+speed = 3
+
 
 while True:
-    im = image1.rotate(0, translate=[i, 0], fillcolor=0xffffff) 
+    # if random.randint % 100 == 0:
+    #     targetX 
+
+    if abs(currentX - targetX) <= speed:
+        currentX = targetX
+    else:
+        currentX += math.copysign(speed, targetX - currentX)
+    if abs(currentY - targetY) <= speed:
+        currentY = targetY
+    else:
+        currentY += math.copysign(speed, targetY - currentY)
+
+
+    im = image1.rotate(0, translate=[currentX, currentY], fillcolor=0xffffff) 
     display1.image(im)
     display2.image(im)
     time.sleep(0.1)
-    i+=1
 
 
