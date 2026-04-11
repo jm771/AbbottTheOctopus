d3ae6c5a86ef0e62582d0339aa578fe7a64a824c
JackMills <JackMills@NotRealEmail.com>
2026-04-11 18:11:49 -0400

    WIP

---
+++ commandline_main.py
@@ -0,0 +1,30 @@
+from time import sleep
+import sys
+import select
+from reaction_state_manager import ReactionStateManager, ReactionType, make_arms_reaction_manager, make_eyes_reaction_manager
+
+ReactionManager = ReactionStateManager(make_eyes_reaction_manager(), make_arms_reaction_manager())
+
+
+def select_reaction(character):
+    if character == 'e':
+        return ReactionType.Excited
+    if character == 'h':
+        return ReactionType.Love
+
+    return None
+
+def readline_nonblocking():
+    if select.select([sys.stdin], [], [], 0)[0]:
+        return sys.stdin.readline().rstrip()
+
+    return None
+
+
+while True:
+    reaction = select_reaction(readline_nonblocking())
+    if reaction is not None:
+        ReactionManager.queue_reaction(reaction)
+    
+    ReactionManager.poll()
+    sleep(0.001)
