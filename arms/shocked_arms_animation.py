6caaca324d0b38706a8d4998137a201e58be3a60
Kenji Garland <kenjigarland@gmail.com>
2026-04-15 14:51:53 -0400

    Reduce magnitude of shock wave

--- arms/shocked_arms_animation.py
+++ arms/shocked_arms_animation.py
@@ -3,11 +3,11 @@ from typing import Optional
 from arms.arm_animation import ArmAnimation
 from arms.arm_controler import ArmController
 
-FULL_CYCLE_LENGTH = 60
+FULL_CYCLE_LENGTH = 120
 N_WAVES = 8
 WAVE_CYCLE_LENGTH = FULL_CYCLE_LENGTH // N_WAVES
 HALF_CYCLE_LENGTH = WAVE_CYCLE_LENGTH // 2
-
+WAVE_MAGNITUDE = 0.25
 
 class ShockedArmsAnimation(ArmAnimation):
     def reset(self):
@@ -18,8 +18,6 @@ class ShockedArmsAnimation(ArmAnimation):
 
     def display_frame(self, left_arm: ArmController, right_arm: ArmController, frame_number: int):
         frame_number -= HALF_CYCLE_LENGTH // 2
-
-        
-        pos = (abs(frame_number % WAVE_CYCLE_LENGTH - HALF_CYCLE_LENGTH)) / HALF_CYCLE_LENGTH / 4
+        pos = (abs(frame_number % WAVE_CYCLE_LENGTH - HALF_CYCLE_LENGTH)) / HALF_CYCLE_LENGTH * WAVE_MAGNITUDE + (1.5 * WAVE_MAGNITUDE)
         left_arm.set_pos(pos)
-        right_arm.set_pos(1-pos)
+        right_arm.set_pos(WAVE_MAGNITUDE-pos)
